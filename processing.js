570de8e5a4ad07c7e24c234616f2bb918fd6c5ff
satya <meetneo1@gmail.com>
2020-08-16 23:13:40 +0200

    First attempt for image analysis

--- processing.js
+++ processing.js
@@ -1,26 +1,58 @@
-function analyzeDistrubtion() {
-    var min = document.getElementById("minValue").value;
-    var max = document.getElementById("maxValue").value;
-
-    var digitCount = new Array(10).fill(0);
-    for (var i = min; i <= max; i++) {
-        var firstDigit = getRandomArbitrary(min, max).toString().substr(0, 1);
-        digitCount[firstDigit] = digitCount[firstDigit] + 1;
-    }
-
-    digitCount.splice(0,1);
-    plotChart(digitCount);
-    var count = 1;
-    digitCount.filter(i => i > 0)
-        .forEach(i => console.log("Digit : " + (count++) + " Count : " + i));
+window.onload = () => {
+    document.getElementById("btnAnalyse").disabled = true;
 }
 
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
+const imageInput = document.getElementById('imageInput')
+imageInput.onchange = function (e) {
+  if (e.target.files && e.target.files.item(0)) {
+    srcImage.src = URL.createObjectURL(e.target.files[0])
+  }
+}
+
+const imageCanvas = document.getElementById('imageCanvas')
+const ctx = imageCanvas.getContext('2d')
+const srcImage = new Image
+
+srcImage.onload = function () {
+  var aspectRatio = 0.50;
+  imageCanvas.width = srcImage.width * aspectRatio
+  imageCanvas.height = srcImage.height * aspectRatio
+  ctx.drawImage(srcImage, 0, 0, srcImage.width * aspectRatio, srcImage.height * aspectRatio)
+  
+  document.getElementById("btnAnalyse").disabled = false;
+}
+
+function analyseImage() {
+    var imageData = ctx.getImageData(0, 0, srcImage.width, srcImage.height)
+    var pix = imageData.data;
+    console.log("Size of pixel : " + pix.length)
+
+    var digitOccArray = new Array(10).fill(0);
+    for (var i = 0; i < pix.length; i += 4) {    
+        var red   = pix[i];
+        var green = pix[i + 1];
+        var blue  = pix[i + 2];
+        var alpha = pix[i + 3];
+
+        var rgba = (red << 24) + (green << 16) + (blue << 8) + (alpha);
+        var firstDigit = rgba.toString().substr(0, 1)
+        
+        digitOccArray[firstDigit] = digitOccArray[firstDigit] + 1;
+    }
+
+    //removing the first element of array ie digitOccArray[0] which is useless
+    digitOccArray.splice(0,1)
+    var counterTotalSize = digitOccArray.reduce((a, b) => a + b, 0)
+    
+    var index = 1;
+    digitOccArray
+        .forEach(digitCount => console.log("Digit : " + (index++) + ", Percentage : " + (digitCount.toFixed(4)/counterTotalSize.toFixed(4)) * 100));
+    
+    plotChart(digitOccArray)
 }
 
 function plotChart(data) {
-    var ctx = document.getElementById('distributionChart');
+    var ctx = document.getElementById('chartCanvas');
     new Chart(ctx, {
         type: 'bar',
         data: {
